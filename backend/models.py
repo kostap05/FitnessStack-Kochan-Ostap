f2c5b2489975e7085c904cc8b44f1990f937716f
Алексей Розеган <rozeganalexey@gmail.com>
2025-06-13 15:24:07 +0200

    Start of work on main app business logic - workouts

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, Float
-from backend.database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship
+from database import Base
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
@@ -14,3 +16,31 @@ class User(Base):
     goal = Column(String, nullable=True)
 
     avatar_url = Column(String, nullable=True)
+
+    workouts = relationship("Workout", back_populates="owner")
+    programs = relationship("Program", back_populates="owner")
+
+
+class Workout(Base):
+    __tablename__ = "workouts"
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime, default=datetime.utcnow)
+    exercise_type = Column(String)
+    duration = Column(Integer)  # in minutes
+    sets = Column(Integer)
+    reps = Column(Integer)
+    calories = Column(Float)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="workouts")
+
+
+class Program(Base):
+    __tablename__ = "programs"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    goal = Column(String)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="programs")
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
+from datetime import datetime
+
 
 class UserRegister(BaseModel):
     username: str
@@ -9,9 +11,11 @@ class UserRegister(BaseModel):
     height: Optional[float] = None
     goal: Optional[str] = None
 
+
 class UserLogin(BaseModel):
     username: str
     password: str
+
 
 class UserOut(BaseModel):
     id: int
@@ -24,6 +28,44 @@ class UserOut(BaseModel):
     class Config:
         orm_mode = True
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
+
+class WorkoutBase(BaseModel):
+    date: Optional[datetime] = None
+    exercise_type: str
+    duration: int
+    sets: int
+    reps: int
+
+
+class WorkoutCreate(WorkoutBase):
+    pass
+
+
+class Workout(WorkoutBase):
+    id: int
+    calories: float
+
+    class Config:
+        orm_mode = True
+
+
+class ProgramBase(BaseModel):
+    name: str
+    goal: str
+    description: Optional[str] = ""
+
+
+class ProgramCreate(ProgramBase):
+    pass
+
+
+class Program(ProgramBase):
+    id: int
+
+    class Config:
+        orm_mode = True
